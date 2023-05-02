98d9390aa692f1ced6d21bbca14c930880f5d3f6
zhangzhankui <zzk13180@gmail.com>
2023-05-02 08:53:57 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  integrations: [compress(), mdx(), tailwind()],
   output: "static",
   site: "https://zzk13180.github.io",
   base: "/passbox",
